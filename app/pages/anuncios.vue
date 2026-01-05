118f8d025438e20c924238414814aa9b2943fd12
francoguilherme <guilhermefranco4@gmail.com>
2026-01-05 12:16:50 -0300

    Aplica texto justificado nas páginas institucionais

--- app/pages/anuncios.vue
+++ app/pages/anuncios.vue
@@ -3,7 +3,7 @@
     <LoadingSpinner v-if="pending" :text="$t('common.loading')" />
     <div v-else-if="error" class="error">{{ $t('common.error_loading', { message: error.message }) }}</div>
     <div v-else class="content">
-      <BlocksRenderer v-if="data?.data?.pagina?.conteudo" :content="data.data.pagina.conteudo" />
+      <BlocksRenderer v-if="data?.data?.pagina?.conteudo" :content="data.data.pagina.conteudo" class="text-justify" />
     </div>
     <ScrollToTop />
   </div>
